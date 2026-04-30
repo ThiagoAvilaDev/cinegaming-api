3e4664ddb8e42b0b8b84b96d417ea129d5d3405f
Thiago Ferreira Avila <thiagotab879@gmail.com>
2026-04-30 18:28:12 -0300

    feat: estrutura inicial Spring boot para CineGaming API

---
+++ src/main/java/br/com/thiago/cinegamingapi/CinegamingapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.cinegamingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinegamingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinegamingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiago/cinegamingapi/CinegamingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.cinegamingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinegamingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
